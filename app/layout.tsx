d756bb43bc1ea354701a56d612ca82bcc74b3bc5
Mri-cia <rawikara8c@gmail.com>
2026-01-16 21:29:03 +0700

    create link for nav, using generic font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
 import Nav from "@/components/nav";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -29,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`font-sans antialiased`}
       >
         <div className="min-h-screen mx-auto max-w-8xl px-10">
           <div className="min-h-screen mx-auto py-5 flex flex-row">
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,9 +1,13 @@
 'use client'
+import Link from "next/link"
 
 const Nav = () => {
     return (
         <div className="w-full border-3 border-zinc-800">
-            tes
+            <span><Link href="/about">About</Link></span>
+            <span><Link href="/dashboard">Dashboard</Link></span>
+            <span><Link href="/my-projects">My Projects</Link></span>
+            <span><Link href="/achivements">Achivements</Link></span>
         </div>
     )
 }
