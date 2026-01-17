f89212b6978a9428ea81ed90c648818bdd063c21
Mri-cia <rawikara8c@gmail.com>
2026-01-17 18:35:19 +0700

    style change nav hover

--- components/nav-tabs.tsx
+++ components/nav-tabs.tsx
@@ -11,7 +11,10 @@ export default function Tabs({ref, label}: {ref: string, label: string}){
         <span className="flex-1">
             <div className={`
                 w-inherit h-inherit p-2 rounded-2xl
-                ${isActive ? "bg-gray-900" : ""}
+                ${isActive ? "bg-gray-900" : `
+                    hover:text-gray-950
+                    hover:bg-gray-100
+                `}
             `}>
                 <Link href={ref} className="flex justify-center font-bold text-xs">{label}</Link>
             </div>
