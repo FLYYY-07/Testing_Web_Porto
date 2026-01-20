b5b07145493f5b76528ed5b26db547c440ac833c
Mri-cia <rawikara8c@gmail.com>
2026-01-20 08:54:01 +0700

    add my-project cards

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function MyProjects(){
     return(
         <main className="flex-1 flex flex-col p-10">
-            <div className="flex-1 flex flex-row flex-wrap gap-x-20 gap-y-10 justify-center scroll-auto">
+            <div className="flex-1 flex flex-row flex-wrap gap-x-10 gap-y-10 justify-center scroll-auto">
                 {[...Array(5)].map((_, i) => (
                     <React.Fragment key={i}>
                         <ProjectCard />
