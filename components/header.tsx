768bccd19c50e2cf1fe14e1635d0b7f0cd9aebe1
Rafli Dharmanda <mraflydharmanda@gmail.com>
2026-01-25 20:29:00 +0700

    edit icon

--- components/header.tsx
+++ components/header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
           {/* 1. Circle Photo dengan sedikit hiasan */}
           <div className="w-30 h-30 bg-gray-600 rounded-full mb-1 border-2 border-gray-500 shadow-lg animate-bounce" style={{alignItems: 'center'}}></div>
           <div style={{alignItems: 'left'}}></div>
-          <span className="text-2xl font-bold mb-20 block">My Portfolio <FcOk className="background-color blue"/>
+          <span className="text-2xl font-bold mb-20 block">My Portfolio
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z "  />
             </svg>
