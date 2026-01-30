537c68c7fa571564da4fc68715d84c31984e38da
Mri-cia <rawikara8c@gmail.com>
2026-01-30 09:50:33 +0700

    clean dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,29 +12,31 @@ export default async function DashboardPage() {
   ]
   
   return (
-    <div className="p-8 text-white bg-[#0d1117] min-h-screen">
-      <h1 className="text-2xl font-bold mb-6">GitHub Contributions 2026</h1>
-      
-      {/* Bagian Statistik Angka (Filter Tahun 2026) */}
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-8">
-        {infos.map((keys) => (
-          <div key={keys.title} className="bg-[#161b22] p-6 rounded-xl border border-gray-800 text-center">
-            <p className="text-gray-400 text-sm mb-1">{keys.title}</p>
-            <p className="text-3xl font-bold text-yellow-400">{keys.data}</p>
-          </div>
-        ))}
-    </div>
+    <div className="p-8">
+      <h1 className="text-2xl font-bold py-3">GitHub Contributions 2026</h1>
 
-      {/* Bagian Grafik Kotak Hijau (Tahun 2026) */}
-      <div className="bg-[#161b22] p-6 rounded-xl border border-gray-800 overflow-hidden">
-        <p className="text-gray-400 mb-4 text-sm text-center">Aktivitas kontribusi kamu di tahun 2026</p>
-        <div className="flex justify-center">
-          <GitHubCalendar 
-            username="FLYYY-07" 
-            theme={{
-              dark: ['#161b22', '#0e4429', '#006d32', '#26a641', '#39d353'],
-            }}
-          />
+      <div className='py-5 gap-5 flex flex-col'>
+        {/* Bagian Statistik Angka (Filter Tahun 2026) */}
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+          {infos.map((keys) => (
+            <div key={keys.title} className="bg-gray-950 p-6 rounded-xl border border-gray-800 text-center">
+              <p className="text-gray-200 text-sm">{keys.title}</p>
+              <p className="text-3xl font-bold text-yellow-400">{keys.data}</p>
+            </div>
+          ))}
+        </div>
+
+        {/* Bagian Grafik Kotak Hijau (Tahun 2026) */}
+        <div className="bg-gray-950 p-2 rounded-xl border border-gray-800 overflow-hidden">
+          <p className="py-3 text-gray-200 text-sm text-center">Aktivitas Selama Setahun</p>
+          <div className="flex justify-center gap-2">
+            <GitHubCalendar 
+              username="FLYYY-07" 
+              theme={{
+                dark: ['#161b22', '#0e4429', '#006d32', '#26a641', '#39d353'],
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
