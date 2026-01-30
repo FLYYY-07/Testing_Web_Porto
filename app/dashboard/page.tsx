49980f72eee5a27cb237410768c0e8337a0c81ab
Mri-cia <rawikara8c@gmail.com>
2026-01-30 09:37:32 +0700

    refactor

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,32 +4,26 @@ import GETGithub from '../_api/github/route';
 export default async function DashboardPage() {
   const data = await GETGithub();
 
+  const infos = [
+    { title: 'Total Commits', data: data?.totalCommitContributions ?? 0},
+    { title: 'Pull Requests', data: data?.totalPullRequestContributions ?? 0},
+    { title: 'Issues', data: data?.totalIssueContributions ?? 0},
+    { title: 'Reviews', data: data?.totalPullRequestReviewContributions ?? 0}
+  ]
+  
   return (
     <div className="p-8 text-white bg-[#0d1117] min-h-screen">
       <h1 className="text-2xl font-bold mb-6">GitHub Contributions 2026</h1>
       
       {/* Bagian Statistik Angka (Filter Tahun 2026) */}
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-8">
-        <div className="bg-[#161b22] p-6 rounded-xl border border-gray-800 text-center">
-          <p className="text-gray-400 text-sm mb-1">Total Commits</p>
-          <p className="text-3xl font-bold text-yellow-400">{data?.totalCommitContributions ?? 0}</p>
-        </div>
-        
-        <div className="bg-[#161b22] p-6 rounded-xl border border-gray-800 text-center">
-          <p className="text-gray-400 text-sm mb-1">Pull Requests</p>
-          <p className="text-3xl font-bold text-yellow-400">{data?.totalPullRequestContributions ?? 0}</p>
-        </div>
-
-        <div className="bg-[#161b22] p-6 rounded-xl border border-gray-800 text-center">
-          <p className="text-gray-400 text-sm mb-1">Issues</p>
-          <p className="text-3xl font-bold text-yellow-400">{data?.totalIssueContributions ?? 0}</p>
-        </div>
-
-        <div className="bg-[#161b22] p-6 rounded-xl border border-gray-800 text-center">
-          <p className="text-gray-400 text-sm mb-1">Reviews</p>
-          <p className="text-3xl font-bold text-yellow-400">{data?.totalPullRequestReviewContributions ?? 0}</p>
-        </div>
-      </div>
+        {infos.map((keys) => (
+          <div key={keys.title} className="bg-[#161b22] p-6 rounded-xl border border-gray-800 text-center">
+            <p className="text-gray-400 text-sm mb-1">{keys.title}</p>
+            <p className="text-3xl font-bold text-yellow-400">{keys.data}</p>
+          </div>
+        ))}
+    </div>
 
       {/* Bagian Grafik Kotak Hijau (Tahun 2026) */}
       <div className="bg-[#161b22] p-6 rounded-xl border border-gray-800 overflow-hidden">
